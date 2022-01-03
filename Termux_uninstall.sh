de726620a881de55ffe00939a83653542b39af17
Coding With Devil <69082359+codingwithdevil@users.noreply.github.com>
2022-01-03 02:00:55 -0500

    Add files via upload

---
+++ Termux_uninstall.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+banner() {
+clear
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m    _____ _                _    \e[0m\e[1;92m _    _ _____  _      \e[0m\n"
+printf "\e[0m\e[1;93m   / ____| |              | |   \e[0m\e[1;92m| |  | |  __ \| |     \e[0m\n"
+printf "\e[0m\e[1;93m  | (___ | |__   ___  _ __| |_  \e[0m\e[1;92m| |  | | |__) | |     \e[0m\n"
+printf "\e[0m\e[1;93m   \___ \|  _ \ / _ \|  __| __| \e[0m\e[1;92m| |  | |  _  /| |     \e[0m\n"
+printf "\e[0m\e[1;93m   ____) | | | | (_) | |  | |_  \e[0m\e[1;92m| |__| | | \ \| |____ \e[0m\n"
+printf "\e[0m\e[1;93m  |_____/|_| |_|\___/|_|   \__| \e[0m\e[1;92m \____/|_|  \_\______|\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m        Created By Coding With Devil\e[0m\e[1;91m\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m    Github :\e[0m\e[1;96m codingwithdevil \e[0m\e[1;97m| \e[0m\e[1;93mInstagram :\e[0m\e[1;96m @codingwithdevil\e[0m\n"
+}
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Uninstalling Packages..\e[0m\n"
+printf "\e[0m\n"
+apt update
+apt install curl -y
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Removing Environment...\e[0m\n"
+printf "\e[0m\n"
+rm -rf /data/data/com.termux/files/usr/bin/codingwithdevil
+rm -rf /data/data/com.termux/files/usr/bin/CODINGWITHDEVIL
+rm -rf /data/data/com.termux/files/usr/bin/instagram
+rm -rf /data/data/com.termux/files/usr/bin/Instagram
+rm -rf /data/data/com.termux/files/usr/bin/shorturl
+rm -rf /data/data/com.termux/files/usr/bin/ShortUrl
+sleep 2
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Uninstallation Succeed !!\e[0m\n"
+sleep 2
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Type \e[0m\e[1;92mThank U For Using \e[0m\e[1;93mRestart Your Device  \e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\n"
